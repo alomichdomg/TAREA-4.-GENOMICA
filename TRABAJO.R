36f90bd7c750707680f6b0d9f26e4a4bff86ffc5
alomichdomg <adominguez09@alumnos.uaq.mx>
2025-04-10 16:16:35 -0600

    ...

--- TRABAJO.R
+++ TRABAJO.R
@@ -1,48 +1,6 @@
 ###PREGUNTAS###
 library(igraph)
-
-calculo_total <- function(grafo){
-#Número de conexiones
-  ecount(grafo)->conexiones
-  print(conexiones)
-  cat("#conexiones:", conexiones, "\n")
-#Número de nodos
-  vcount(grafo)->nodos
-  print(nodos)
-  cat("#nodos:", nodos, "\n")
-  
-  #Degree
-degree(grafo, mode = "total")->total
-print(total)
-cat("#degree:", total, "\n")
-
-#Average degree
-ava_degree <- total/nodos
-print(ava_degree)
-cat("degreeavarage:", ava_degree, "\n")
-#Degree distribution
-  hist(total, col="purple", main="Histograma del grado")
-#Density
-densi <- conexiones/(nodos * (nodos-1))
-print(densi)
-cat("densidad:", densi, "\n")
-#Adjacency matrix
-adj_mat <- as.matrix(as_adjacency_matrix(grafo))
-View(adj_mat)
-#Matriz de distancia
-distances(grafo)
-dist_matrix <- distances(grafo)
-heatmap(dist_matrix, main="Matriz de distancias")
-
-#Diámetro
-diameter(grafo)->diametro
-print(diametro)
-cat("diametro:", diametro, "\n")
-#Coeficiente de clusterización
-transitivity(grafo, type = "local")->cluster_1
-print(cluster_1)
-cat("cluster local:", cluster_1, "\n")
-}
+source("FUNCION.R")
 
 ###RED 1
 red1 <- make_graph(edges = c(6,10, 6,9, 6,4, 4,8, 4,2, 2,1, 2,3, 3,5, 1,7), directed = FALSE)
@@ -75,6 +33,52 @@ red6 <- make_graph(edges = c(4,9,4,8,2,4,2,5,5,10,1,2,1,3,3,7,3,6),
 plot(red6)
 calculo_total(red6)
 #################################################################################
+#KARATE:
 karate<-make_graph("Zachary")
+
+#PREGUNTA 7
 calculo_total(karate)
 mean_distance(karate)
+
+#PREGUNTA DEL DEGREE DISTRIBUTION
+degree_distribution(karate)->degreed
+hist(degreed, , col="purple", main="Histograma del grado")
+
+
+#PREGUNTA 8
+distancia_1 <- distances(karate)
+print(distancia_1)
+
+max(distancia_1)->distancia_maxima
+which(distancia_1 == distancia_maxima)#
+
+#la trayectoria mas lejana es de 5 y son estos los nodos
+#PREGUNTA 10
+  #1.-Centralidad de grados:
+degree(karate) 
+V(karate)$degree <- degree(karate) # assignment
+
+plot(karate, vertex.label.cex = .6, vertex.label.color = "black", vertex.size = V(karate)$degree, vertex.label.cex = .2) # sized by degree
+  #2.- Centralidad de intermediación
+betweenness(karate)#, directed = TRUE)
+V(karate)$betweenness <- betweenness(karate)#, directed = F) # assignment
+
+plot(karate, 
+     vertex.label.cex = .6, 
+     vertex.label.color = "black", 
+     vertex.size = V(karate)$betweenness) # sized by betweenness
+  #3.- Centralidad de cercania
+closeness(karate)
+V(karate)$closeness <- closeness(karate)
+plot(karate,
+     vertex.label.cex = .6, 
+     vertex.label.color = "black", 
+     vertex.size = V(karate)$closeness/max(V(karate)$closeness) * 20)
+  #4.- Centralidad del vector propio.
+eigen_centrality(karate)$vector
+V(karate)$eigenvector <- eigen_centrality(karate)$vector
+
+plot(karate,
+     vertex.label.cex = .6, 
+     vertex.label.color = "black", 
+     vertex.size = V(karate)$eigenvector/max(V(karate)$eigenvector) * 20)
