929f1ea04e377952279beec36fff014b04f82301
alomichdomg <adominguez09@alumnos.uaq.mx>
2025-04-10 20:29:07 -0600

    ,,,,

--- REDES BIOLOGICAS.R
+++ REDES BIOLOGICAS.R
@@ -1,13 +1,24 @@
 ###4.  **Redes biológicas**####
-  
-  Ve a al [Network Repository](https://networkrepository.com/bn-mouse-visual-cortex-1.php) y descarga la red en formato lista de adyacencia. Explica que representam los nodos y las conectividades.
+library(igraph)
 
-Escribe código que resuelva lo siguiente:
+#Cargue y genere una gráfica en `igraph`.
+red_biologica <- read.table("bn-mouse_visual-cortex_1.edges") #es una tabla
+
+red_bio<- graph_from_data_frame(red_biologica)
+
+plot(red_bio, vertex.size=12,vertex.size=5,
+     edge.arrow.size=0.25,layout=layout_nicely,vertex.size.label=0.25)
+
+#Explica que representam los nodos y las conectividades.
+
   
-  -   Cargue y genere una gráfica en `igraph`.
--   Genera la gráfica con todos los layouts disponibles.
--   Grafica la red con al menos tres medidas de centralidad.
+Genera la gráfica con todos los layouts disponibles.
+
+#Grafica la red con al menos tres medidas de centralidad.
 -   ¿Qué tan densa es la red?
   -   Clusteriza la red con al menos tres métodos de clusterización. Gráfica la red con esos métodos.
 
 Explica tus resultados.
+
+#REFERENCIA:https://stackoverflow.com/questions/58350400/how-to-read-in-edges-file-in-r
+################################################################################
